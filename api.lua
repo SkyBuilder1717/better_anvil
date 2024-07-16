08993da3383a255a48e5c74127bd872d70e328f7
BlackImpostor <106878493+SkyBuilder1717@users.noreply.github.com>
2024-07-16 23:00:50 +0300

    source import

---
+++ api.lua
@@ -0,0 +1,98 @@
+--==API DOCUMENTATION==--
+--[[
+
+    better_anvil.register_repair() needs to api of mod for repairing items in node
+    
+    -How to use?
+    --better_anvil.register_repair(name, def)
+      --[
+       NAME: real name of item (CRAFTITEM!!)
+       DEF: table that must contains:
+       {
+        item = "default:diamond" -- ITEM THAT WILL REPAIR OUR TOOL IN "NAME"
+       }
+      ]--
+
+]]--
+
+function better_anvil.register_repair(name, def)
+    better_anvil.registered_repairs[name] = def
+end
+
+function better_anvil.get_formspec(x, nm)
+    local formspec
+    if nm == nil then
+        nm = ""
+    end
+    if x then
+        formspec = "formspec_version[6]"..
+        "size[10.5,12]"..
+        "list[context;input;1.1,5.1;1,1;0]"..
+        "label[4.8,2.5;Anvil]"..
+        "image[4.1,0.1;2.1,2.1;better_anvil_icon.png]"..
+        "image[6.6,5.1;1,1;gui_furnace_arrow_bg.png^[transformR270]]"..
+        "list[context;output;8.4,5.1;1,1;0]"..
+        "list[context;modifier;4.7,5.1;1,1;0]"..
+        "image[6.5,5;1.2,1.2;server_incompatible.png]"..
+        "image[2.9,5.1;1,1;gui_better_anvil_plus_bg.png]"..
+        "field[2.9,3.2;4.7,0.8;name;Name;"..nm.."]"..
+        "list[current_player;main;0.4,6.9;8,4;0]"..
+        "button[3.7,4.1;3,0.6;upd;Update]"
+    else
+        formspec = "formspec_version[6]"..
+        "size[10.5,12]"..
+        "list[context;input;1.1,5.1;1,1;0]"..
+        "label[4.8,2.5;Anvil]"..
+        "image[4.1,0.1;2.1,2.1;better_anvil_icon.png]"..
+        "image[6.6,5.1;1,1;gui_furnace_arrow_bg.png^[transformR270]]"..
+        "list[context;output;8.4,5.1;1,1;0]"..
+        "list[context;modifier;4.7,5.1;1,1;0]"..
+        "image[2.9,5.1;1,1;gui_better_anvil_plus_bg.png]"..
+        "field[2.9,3.2;4.7,0.8;name;Name;"..nm.."]"..
+        "list[current_player;main;0.4,6.9;8,4;0]"..
+        "button[3.7,4.1;3,0.6;upd;Update (PRESS)]"..
+        "label[7.67,6.35;(You can take output)]"
+    end
+    return formspec
+end
+
+function better_anvil.update(pos, fields)
+    local meta = minetest.get_meta(pos)
+    local inv = meta:get_inventory()
+    local instack = inv:get_stack("input", 1)
+    local mdstack = inv:get_stack("modifier", 1)
+    local imeta = instack:get_meta()
+    local nm = instack:get_description()
+    local mnm = mdstack:get_name()
+    local name
+    if not instack:is_empty() then
+        if not (fields.name == "" or fields.name == " ") then
+            name = fields.name
+        else
+            name = nm
+        end
+        local desc = minetest.registered_items[instack:get_name()].description
+        if not fields.name == desc then
+            imeta:set_string("description", core.colorize("lightgrey", name))
+        end
+        local wear = instack:get_wear() - (mdstack:get_wear() / 3)
+        if wear > 65535 then wear = 65535 end
+        if mdstack:is_empty() then
+            meta:set_string("formspec", better_anvil.get_formspec(true, name))
+        else
+            if instack:get_name() == mdstack:get_name() then
+                meta:set_string("formspec", better_anvil.get_formspec(false, name))
+                instack:set_wear(wear)
+            elseif mdstack:get_definition().groups.dye == 1 then
+                meta:set_string("formspec", better_anvil.get_formspec(false, name))
+                imeta:set_string("color", mname)
+            else
+                meta:set_string("formspec", better_anvil.get_formspec(true, name))
+            end
+        end
+        inv:set_stack("output", 1, instack)
+    else
+        inv:set_stack("output", 1, "")
+        meta:set_string("formspec", better_anvil.get_formspec(true))
+    end
+end
---
+++ init.lua
@@ -0,0 +1,102 @@
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+better_anvil = {
+    registered_repairs = {}
+}
+dofile(modpath.."/api.lua")
+
+minetest.register_node("better_anvil:anvil", {
+    description = "Anvil",
+	tiles = {
+		"better_anvil_top.png",
+		"better_anvil_top.png",
+		"better_anvil_side1.png",
+		"better_anvil_side1.png",
+		"better_anvil_side2.png",
+		"better_anvil_side2.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3125, 0.0625, -0.4375, 0.375, 0.5, 0.4375},
+			{-0.1875, -0.4375, -0.25, 0.25, 0.4375, 0.25},
+			{-0.375, -0.5, -0.4375, 0.4375, -0.1875, 0.4375},
+		}
+	},
+    groups = {cracky = 1, falling_node = 1, level = 1},
+	sounds = default.node_sound_metal_defaults(),
+    on_construct = function(pos)
+        local meta = minetest.get_meta(pos)
+        if meta == nil then
+            return
+        end
+        meta:set_string("formspec", better_anvil.get_formspec(true))
+        local inv = meta:get_inventory()
+        inv:set_size("input", 1)
+        inv:set_size("modifier", 1)
+        inv:set_size("output", 1)
+    end,
+	on_metadata_inventory_take = function(pos, list_name, i, stack, player)
+		local meta = minetest.get_meta(pos)
+	    local inv = meta:get_inventory()
+		local mstack = inv:get_stack("modifier", 1)
+		if list_name == "output" and mdstack:get_name() == "" then
+			inv:set_stack("input", 1, "")
+            if (mdstack:get_definition().groups.dye == 1) or (instack:get_name() == mdstack:get_name()) then
+				inv:set_stack("modifier", 1, "")
+            end
+			meta:set_string("formspec", better_anvil.get_formspec(true))
+		elseif list_name == "modifier" then
+			inv:set_stack("output", 1, "")
+		end
+	end,
+	allow_metadata_inventory_put = function(pos, list_name, index, itemstack, player)
+		local meta = minetest.get_meta(pos)
+	    local inv = meta:get_inventory()
+		if list_name == "input" then
+			local name = itemstack:get_name()
+			meta:set_string("formspec", better_anvil.get_formspec(false))
+			return 1
+		elseif list_name == "modifier" then
+			local istack = inv:get_stack("input", 1)
+			local name = itemstack:get_name()
+			if istack then
+				local craft = minetest.get_craft_recipe(name)
+				minetest.chat_send_all(dump(craft))
+				if istack:get_name() == name then
+					if (istack:get_wear() < itemstack:get_wear()) or (istack:get_wear() == itemstack:get_wear()) then
+						return 1
+					else
+						return 0
+					end
+				elseif itemstack:get_definition().groups.dye == 1 then
+					return 1
+				else
+					return 1
+				end
+			else
+				meta:set_string("formspec", better_anvil.get_formspec(true))
+				return 1
+			end
+		elseif list_name == "output" then
+			meta:set_string("formspec", better_anvil.get_formspec(true))
+			return 0
+		end
+		better_anvil.update(pos, fields)
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		better_anvil.update(pos, fields)
+	end,
+})
+minetest.register_alias("anvil", "better_anvil:anvil")
+
+minetest.register_craft({
+	output = "better_anvil:anvil",
+	recipe = {
+		{"default:steelblock", "default:steelblock", "default:steelblock"},
+		{"", "default:steel_ingot", ""},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
+	}
+})
